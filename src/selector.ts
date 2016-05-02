6b4ff8b2f7edb615eaa611593c4f5bc19ad845e6
Ian MacLeod <ian@nevir.net>
2016-05-02 11:55:17 -0700

    Implementation of @selector

---
+++ src/selector.ts
@@ -0,0 +1,36 @@
+import * as _ from 'lodash';
+
+type Selector = (state:Object, ...args:any[]) => any;
+
+// TODO: https://github.com/Microsoft/TypeScript/issues/4881
+/**
+ * Shorthand for a selector that scopes down to the mount point of interactions.
+ *
+ * For example:
+ *
+ *   class Todos extends Interactions {
+ *     initialState = [];
+ *
+ *     mountPoint = ['entities', 'todos'];
+ *
+ *     @selector
+ *     getFirst(scopedState) {
+ *       return scopedState[0];  // Returns storeState.entities.todos[0]
+ *     }
+ *   }
+ *
+ */
+export default function selector(_target:Object, _key?:string, descriptor?:PropertyDescriptor):any {
+  if (typeof descriptor.value !== 'function') {
+    throw new TypeError(`@selector can only decorate methods`);
+  }
+
+  descriptor.value = _makeSelector(descriptor.value);
+}
+
+function _makeSelector(scopedSelector:(scopedState:Object, ...args:any[]) => any):Selector {
+  return function interactionsSelector(state:Object, ...args:any[]):Selector {
+    // We rely on interactions objects auto-binding all methods (for `this`).
+    return scopedSelector(_.get(state, this.mountPoint), ...args);
+  };
+}
