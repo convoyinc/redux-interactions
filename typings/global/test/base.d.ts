4c4b8783f913393778568b13ded85afea3bdc5b1
Rylan Hawkins <rylan@convoy.com>
2018-02-12 08:50:24 -0800

    Add global typings

---
+++ typings/global/test/base.d.ts
@@ -0,0 +1,12 @@
+import * as chai from 'chai';
+
+declare global {
+  const expect: typeof chai.expect;
+
+  namespace NodeJS {
+    export interface Global {
+      expect: typeof chai.expect;
+      document: any;
+    }
+  }
+}
